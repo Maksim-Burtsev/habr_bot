df148dc007403249ba1c15532e56449e0d2a5fdc
MaximBurtsev <zadrot-lol@list.ru>
2022-09-15 08:53:54 +0300

    refactor: move funcs from main to services; add func for today articles

--- main.py
+++ main.py
@@ -1,57 +1,33 @@
 import os
-import time
 
-import telebot
-from telebot import types
-from telegram import ParseMode
+from telebot import TeleBot
 from dotenv import load_dotenv
 
-from services import get_current_msk_date, Article, HabrParser
+import services
 
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-bot = telebot.TeleBot(TOKEN)
-parser = HabrParser()
 
-
-def send_articles(message: types.Message, articles: list[Article]) -> None:
-    for article in articles:
-        time.sleep(0.333)
-        bot.send_message(
-            chat_id=message.chat.id, text=article.as_hypelink, parse_mode=ParseMode.HTML
-        )
-
-
-def create_markup() -> types.ReplyKeyboardMarkup:
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("все публикации с главной страницы")
-    item2 = types.KeyboardButton("все посты за сегодня")
-    markup.add(item1)
-    markup.add(item2)
-
-    return markup
+bot = TeleBot(TOKEN)
+parser = services.HabrParser()
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    markup = create_markup()
+    markup = services.create_markup()
     bot.send_message(message.chat.id, "Привет", reply_markup=markup)
 
 
 @bot.message_handler(content_types="text")
 def get_articles(message):
     if message.text == "все публикации с главной страницы":
-        articles = parser.get_articles(pages_amount=1)
-        send_articles(message, articles)
+        articles = parser.get_articles()
+        services.send_articles(bot, message, articles)
 
     elif message.text == "все посты за сегодня":
-        articles = parser.get_articles(pages_amount=3)
-        current_date = get_current_msk_date()
-        daily_articles = [
-            article for article in articles if article.date == current_date
-        ]
-        send_articles(message, daily_articles)
+        articles = parser.get_today_articles()
+        services.send_articles(bot, message, articles)
 
 
 if __name__ == "__main__":
--- services.py
+++ services.py
@@ -1,9 +1,12 @@
+import time
 from enum import Enum
 from datetime import datetime, timezone, timedelta
 
 import requests
 import fake_useragent
 from bs4 import BeautifulSoup
+from telegram import ParseMode
+from telebot import types, TeleBot
 from pydantic.dataclasses import dataclass
 
 
@@ -20,14 +23,6 @@ class URL(str, Enum):
     HABR_NEWS = "https://habr.com/ru/all/"
 
 
-def get_current_msk_date() -> str:
-    """Return current date in Europe/Moscow."""
-    delta = timedelta(hours=3, minutes=0)
-    current_datetime = datetime.now(timezone.utc) + delta
-
-    return str(current_datetime)[:10]  # 2022-03-10
-
-
 @dataclass
 class Article:
     title: str
@@ -40,6 +35,38 @@ class Article:
 
     def __repr__(self) -> str:
         return f"«{self.title}»"
+
+
+def get_current_msk_date() -> str:
+    """Return current date in Europe/Moscow."""
+    delta = timedelta(hours=3, minutes=0)
+    current_datetime = datetime.now(timezone.utc) + delta
+
+    return str(current_datetime)[:10]  # 2022-03-10
+
+
+def send_articles(
+    bot: TeleBot, message: types.Message, articles: list[Article]
+) -> None:
+    """Send articles to user, 1 article = 1 message.
+
+    Using sleep to slow sending."""
+    for article in articles:
+        time.sleep(0.333)
+        bot.send_message(
+            chat_id=message.chat.id, text=article.as_hypelink, parse_mode=ParseMode.HTML
+        )
+
+
+def create_markup() -> types.ReplyKeyboardMarkup:
+    """Create buttons for menu"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("все публикации с главной страницы")
+    item2 = types.KeyboardButton("все посты за сегодня")
+    markup.add(item1)
+    markup.add(item2)
+
+    return markup
 
 
 class HabrParser:
@@ -84,8 +111,22 @@ class HabrParser:
 
         return Article(title, URL.HABR_DEFAULT.value + url, date)
 
-    def get_articles(self, pages_amount: int) -> list[Article]:
-        """about this and reverse return"""
+    def get_today_articles(self) -> list[Article]:
+        """Return list of today's articles.
+
+        Parse first three pages because to make sure no article was missed today."""
+        articles = self.get_articles(pages_amount=3)
+        current_date = get_current_msk_date()
+        daily_articles = [
+            article for article in articles if article.date == current_date
+        ]
+
+        return daily_articles
+
+    def get_articles(self, pages_amount: int = 1) -> list[Article]:
+        """Return reversed list of articles (from new to old).
+
+        By default get articles from main page."""
         raw_articles = []
         for page_num in range(1, pages_amount + 1):
             url = f"{URL.HABR_NEWS.value}page{str(page_num)}/"
